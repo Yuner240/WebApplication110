81e127549895f8d2178d72675a181db113057cf0
Yuner Ch <orozcochaconyuner@gmail.com>
2025-05-04 22:34:30 -0600

    Agregar archivos de proyecto.

---
+++ Pages/login2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication110.Pages
+{
+    public class login2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
